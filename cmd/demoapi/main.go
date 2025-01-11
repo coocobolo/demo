a08eb9836df96ba1e2c746aceda72a5a35f9199f
Kbgjtn <kambingjan31@gmail.com>
2025-01-11 09:45:43 +0000

    refactor: compose custom container name

--- cmd/demoapi/main.go
+++ cmd/demoapi/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	hostName := os.Getenv("HOSTNAME")
+	if hostName == "" {
+		hostName = "unknown"
+	}
+
 	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("ok"))
@@ -18,7 +25,8 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("hello-world"))
+		msg := fmt.Sprintf("hello-world from %s", hostName)
+		w.Write([]byte(msg))
 	})
 
 	server := http.Server{
